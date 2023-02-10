98d6f1935b8f6cbe25b71b888f093d345916d9a0
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2023-02-10 16:19:28 -0500

    Update GriffNuke.py

--- GriffNuke.py
+++ GriffNuke.py
@@ -1,14 +1,22 @@
 import scratchattach as scratch3, random
-random.randint(20, 100)\nwhile True:
+random.randint(20, 100)
+
+while True:
     print()
     try:
-        user = scratch3.get_user(input("Please provide the profile to scan: "))\n    except Exception as e:
+        user = scratch3.get_user(input("Please provide the profile to scan: "))
+
+    except Exception as e:
         print(str(e))
         while True:
             pass
-    print("Agent #" + str(random.randint(20, 100)), "is on the job.\nLoading comments..")\n    print()
+    print("Agent #" + str(random.randint(20, 100)), "is on the job.\nLoading comments..")
+
+    print()
     comments = user.comments(limit=40, page=1)
-    print("Filtering comments..\n")\n    try:
+    print("Filtering comments..\n")
+
+    try:
         for target in comments:
             author = target["User"]
             target = target["Content"].lower()
@@ -16,5 +24,9 @@ random.randint(20, 100)\nwhile True:
                 print()
                 # print("ADS DETECTED: ")
                 print("AUTHOR:", author)
-                print("CONTENTS:", target)\n                print()\n    except Exception as e:
+                print("CONTENTS:", target)
+
+                print()
+
+    except Exception as e:
         print(str(e))
